e2a620d786d1a2af8a949228aae6bc0dff6cce89
tkingless <t.kingless@yahoo.com>
2018-10-24 15:39:37 +0800

    can load custom Dartboard.storyboard

--- micro-bit template.playgroundbook/Contents/Sources/View Controllers/DartboardViewController.swift
+++ micro-bit template.playgroundbook/Contents/Sources/View Controllers/DartboardViewController.swift
@@ -5,4 +5,21 @@
 //  Created by tkingless on 24/10/2018.
 //
 
-import Foundation
+import UIKit
+import CoreBluetooth
+import PlaygroundSupport
+
+@objc(DartboardViewController)
+public class DartboardViewController: UIViewController, PlaygroundLiveViewSafeAreaContainer {
+    
+    public static func controllerFromStoryboard(_ storyboardName: String) ->
+        DartboardViewController {
+        
+        let bundle = Bundle(for: DartboardViewController.self)
+        let storyboard = UIStoryboard(name: storyboardName, bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! DartboardViewController
+        
+        return controller
+    }
+    
+}
